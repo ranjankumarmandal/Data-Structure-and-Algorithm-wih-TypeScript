e65a07e1c6c7fbb85096b7aa73e51ab68640d029
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-07 16:49:29 +0530

    added some definitions and details for time complexity

---
+++ 01_bigO_notation/z_very_important.ts
@@ -0,0 +1,13 @@
+/*
+
+Time Complexity -> Its the time required by computer to run an algorithm.
+O(n) - Its the worst case time complexity notation, and it is widely used than other types due to its power.
+
+1. Arithmetic operations are constant. ie, O(1)
+2. Variable assignment is constant. ie, O(1)
+3. Accessing element of an array with index or of an object with key. Time complexity is constant O(1)
+4. In a loop - Time complexity depends on length of loop. ie, it may be O(n), O(n^2), or O(n^3), etc.
+
+... and so on.
+
+*/
