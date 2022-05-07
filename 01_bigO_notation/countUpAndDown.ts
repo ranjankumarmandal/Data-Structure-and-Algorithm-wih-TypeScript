48b0bab4cf8f470189d893c165360c4de0cffb46
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-07 15:35:46 +0530

    created a solution for problem  counting a number from up an down

---
+++ 01_bigO_notation/countUpAndDown.ts
@@ -0,0 +1,7 @@
+function countUpAndDown(n: number): void {
+  console.log('Going Up!');
+  for (let i = 0; i <= n; i++) console.log(i);
+
+  console.log('Goind Down!');
+  for (let j = n; j >= 0; j--) console.log(j);
+}
