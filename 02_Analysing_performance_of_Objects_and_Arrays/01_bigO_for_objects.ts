33046342d09ec286cd63590c587946ebd8befff6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-07 17:28:43 +0530

    updated definition

--- 02_Analysing_performance_of_Objects_and_Arrays/01_bigO_for_objects.ts
+++ 02_Analysing_performance_of_Objects_and_Arrays/01_bigO_for_objects.ts
@@ -1,1 +1,1 @@
-// Objects - these are un-ordered datastructure. Here everything is stored in key-value pair
+// Objects - these are un-ordered datastructure. Here everything is stored in key-value pair. If you don't need order, objects are an excellent choice!
